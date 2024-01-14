911fc5a630e343c624a54ad7fe8026d779811bde
Keita58 <marc-s-26@hotmail.com>
2024-01-14 09:32:19 +0100

    Codificant el codi per a resoldre l'exercici.

--- src/JUnit/Problema.java
+++ src/JUnit/Problema.java
@@ -1,6 +1,5 @@
 package JUnit;
 
-import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class Problema {
@@ -9,16 +8,16 @@ public class Problema {
 		Scanner ohowo = new Scanner(System.in);
 		
 		int f = ohowo.nextInt();
-		int c = ohowo.nextInt();
-		int[][] matriu = new int[f][c];
+		int[][] matriu = new int[f][f];
 		
 		for(int i = 0; i < f; i++) {
-			for(int j = 0; j < c; j++) {
+			for(int j = 0; j < f; j++) {
 				matriu[i][j] = ohowo.nextInt();
 			}
 		}
 		
 		int[] res = posicio(matriu);
+		
 	}
 
 	public static int[] posicio(int[][] matriu) {
--- src/JUnit/TestExercici.java
+++ src/JUnit/TestExercici.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class proves {
+class TestExercici {
 
 	@Test
 	void test() {
